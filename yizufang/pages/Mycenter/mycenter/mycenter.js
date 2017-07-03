380847973b41e0772c2624b0b3bc746035beb5c7
左佰全 <zuobaiquan@ipzoe.com>
2017-07-03 09:51:38 +0800

    update

--- yizufang/pages/Mycenter/mycenter/mycenter.js
+++ yizufang/pages/Mycenter/mycenter/mycenter.js
@@ -1,11 +1,29 @@
 
 const wechat = require('../../../utils/wechat.js')
+var app = getApp()
 
 Page({
   data: {
+    currentTab: 1
+  },
+
+  onLoad(options) {
+    
+  },
+  
+  selectedTab: function (e) {
+    var index = e.currentTarget.dataset.current;
+    var that = this;
+    if (this.data.currentTab === index) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: index
+      })
+    }
+
 
   },
-  onLoad() {
-    console.log(11);
-  }
+
 })
+
