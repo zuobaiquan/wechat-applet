0391d50d18869158048ad513ef277aba5a4c64b4
左佰全 <zuobaiquan@ipzoe.com>
2017-07-16 23:27:08 +0800

    update

--- 北美易租/pages/Index/index/index.js
+++ 北美易租/pages/Index/index/index.js
@@ -3,7 +3,10 @@ const wechat = require('../../../utils/wechat.js')
 
 Page({
   data: {
-    swiperList: [{ num: 888, title: "华盛顿大学", url: '../images/middle-img1.jpg' }, { num: 999, title: "西雅图", url: '../images/middle-img2.jpg' }, { num: 666, title: "西雅图2", url: '../images/middle-img2.jpg' }],
+    swiperList: [
+       { num: 888, title: "华盛顿大学", url: '../images/middle-img1.jpg' }, 
+       { num: 999, title: "西雅图", url: '../images/middle-img2.jpg' }, 
+       { num: 666, title: "西雅图2", url: '../images/middle-img2.jpg' }],
     houseList: [{ title: 'U-District附近三层别墅', isCollect: 1, url: '../images/houselist1.png', price: "999 USD/月", type: '独栋别墅', addr: 'Seattle，WA', detail: '4卧 2卫浴' }, { title: '首页房源标题2', isCollect: 0,url:'../images/houseno-img.png', price: "2199999 USD/天", type: '联排别墅', addr: 'Seattle，WA', detail: '3卧 1卫浴' }],
     rentMate: [{ url: 'http://p1.qzone.la/Upload/20160402/20160402165345391387.gif', isCollect: 0, nickname: '小明', city: '西雅图', price: '1000 USD/月', startTime: '2017-06-01', endTime: '2018-06-01', req: '没有任何要求' }, { url: 'http://p1.qzone.la/Upload/20160402/20160402165345391387.gif', isCollect: 1, nickname: '大明',  city: '西雅图22', price: '1200 USD/月', startTime: '2017-06-12', endTime: '2018-06-18', req: '有任何要求' }],
     roomMate: [{ url: 'http://p1.qzone.la/Upload/20160402/20160402165345391387.gif', isCollect: 1, nickname: '小明', city: '西雅图', sex: '本人女,希望室友性别女', price: '1000 USD/月', startTime: '2017-06-01', req: '我喜欢猫的，希望x喜欢猫' }, { url: 'http://p1.qzone.la/Upload/20160402/20160402165345391387.gif', isCollect: 1, nickname: '大明', city: '西雅图22', sex: '本人男,希望室友性别男', price: '1200 USD/月', startTime: '2017-06-12', endTime: '2018-06-18', req: '有任何要求' }, { url: 'http://p1.qzone.la/Upload/20160402/20160402165345391387.gif', isCollect: 1, nickname: '大明', city: '西雅图22', sex: '本人男,希望室友性别男', price: '1200 USD/月', startTime: '2017-06-12', endTime: '2018-06-18', req: '有任何要求' }],
@@ -71,7 +74,7 @@ Page({
       url: '../../Mycenter/housedetail/housedetail'
     })
   },
-  detailDent(){
+  detailRent(){
     wx.navigateTo({
       url: '../../Community/rentdetail/rentdetail?type=2&look=false'
     })
--- 北美易租/pages/Mycenter/mycenter/mycenter.js
+++ 北美易租/pages/Mycenter/mycenter/mycenter.js
@@ -1,48 +1,45 @@
 
 Page({
   data: {
+    delBtnWidth: 182,
     chartlist: [
-       { type: 1,status:1, avator: 'https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=2406161785,701397900&fm=5', title: 'UW附近2b1b整套出租', url: '../images/houselist1.png', price: "999 USD/月", typename: '独栋别墅', addr: 'Seattle，WA', detail: '4卧 2卫浴' },
-       { type: 1, status: 0, avator: 'https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=2406161785,701397900&fm=5', title: '哈哈哈近2b1b整套出租', url: '../images/houselist1.png', price: "888 USD/月", typename: '独栋别墅', addr: 'Seattle，WA', detail: '2卧 2卫浴' },
-       { type: 1, status: 1, avator: 'https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=2406161785,701397900&fm=5', title: '哈哈哈近2b1b整套出租', url: '../images/houselist1.png', price: "888 USD/月", typename: '独栋别墅', addr: 'Seattle，WA', detail: '2卧 2卫浴' },
-       { type: 2, status: 0, city: '西雅图0', sex: '本人女希望室友性别女0', price: "600 USD/月", startTime: '2017-06-01', req: '我喜欢猫的，希望x喜欢猫' },
-       { type: 2, status: 1, city: '西雅图1', sex: '本人女希望室友性别女1', price: "600 USD/月", startTime: '2017-06-01', req: '我喜欢猫的，希望x喜欢猫' }
+      { txtStyle: '', type: 1,status:1, avator: 'https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=2406161785,701397900&fm=5', title: 'UW附近2b1b整套出租', url: '../images/houselist1.png', price: "999 USD/月", typename: '独栋别墅', addr: 'Seattle，WA', detail: '4卧 2卫浴' },
+      { txtStyle: '', type: 1, status: 0, avator: 'https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=2406161785,701397900&fm=5', title: '哈哈哈近2b1b整套出租', url: '../images/houselist1.png', price: "888 USD/月", typename: '独栋别墅', addr: 'Seattle，WA', detail: '2卧 2卫浴' },
+      { txtStyle: '', type: 1, status: 1, avator: 'https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=2406161785,701397900&fm=5', title: '哈哈哈近2b1b整套出租', url: '../images/houselist1.png', price: "888 USD/月", typename: '独栋别墅', addr: 'Seattle，WA', detail: '2卧 2卫浴' },
+      { txtStyle: '', type: 2, status: 0, city: '西雅图0', sex: '本人女希望室友性别女0', price: "600 USD/月", startTime: '2017-06-01', req: '我喜欢猫的，希望x喜欢猫' },
+      { txtStyle: '', type: 2, status: 1, city: '西雅图1', sex: '本人女希望室友性别女1', price: "600 USD/月", startTime: '2017-06-01', req: '我喜欢猫的，希望x喜欢猫' },
+      { txtStyle: '', type: 3, status: 1, city: '西雅图1', price: "600 USD/月", startTime: '2017-06-01', endTime: '2017-12-01', req: '我喜欢猫的，希望x喜欢猫' }
      ],
-    // 页面配置  
-    winWidth: 0,
-    winHeight: 0,
+    collectList:[
+      { type: 1, status: 1, avator: 'https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=2406161785,701397900&fm=5', title: 'UW附近2b1b整套出租', url: '../images/houselist1.png', price: "999 USD/月", typename: '独栋别墅', addr: 'Seattle，WA', detail: '4卧 2卫浴' },
+      { type: 1, status: 1, avator: 'https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=2406161785,701397900&fm=5', title: '哈哈哈近2b1b整套出租', url: '../images/houselist1.png', price: "888 USD/月", typename: '独栋别墅', addr: 'Seattle，WA', detail: '2卧 2卫浴' },
+      { type: 2, status: 1, city: '西雅图0', sex: '本人女希望室友性别女0', price: "600 USD/月", startTime: '2017-06-01', req: '我喜欢猫的，希望x喜欢猫' }
+    ],
     // tab切换 
     currentTab: 0,
     showrentDetail: false
   },
-  onLoad() {
-    var that = this;
-    that.setData({
-      winWidth: '100%',
-      winHeight: that.data.chartlist.length*366
-    });
+  onLoad(){
+    this.initEleWidth();
   },
-  bindChange: function (e) {
+  selectedTab(e) {
+    var index = e.currentTarget.dataset.current;
     var that = this;
-    that.setData({ currentTab: e.detail.current });
-  },
-  swichNav: function (e) {
-    var that = this;
-    if (this.data.currentTab === e.target.dataset.current) {
+    if (this.data.currentTab === index) {
       return false;
     } else {
       that.setData({
-        currentTab: e.target.dataset.current
+        currentTab: index
       })
     }
   },
 
-  showrentDetail: function () {
+  showrentDetail() {
     this.setData({
       showrentDetail: !this.data.showrentDetail
     });
   },
-  hiderentDetail: function () {
+  hiderentDetail() {
     this.setData({
       showrentDetail: false
     });
@@ -56,9 +53,146 @@ Page({
       case 3: url = '../addhouse/addhouse'; break;
       default: console.log('错误');
     }
+    this.setData({
+      showrentDetail: false
+    });
     wx.navigateTo({
       url: url
     })
+  },
+  detailHouse() {
+    wx.navigateTo({
+      url: '../../Mycenter/housedetail/housedetail'
+    })
+  },
+  detailRentroom(e){
+    const listtype = parseInt(e.currentTarget.dataset.listtype);
+    console.log(e,listtype);;
+    if (listtype==3){
+      wx.navigateTo({
+        url: '../../Community/rentdetail/rentdetail?type=2&look=false'
+      })
+    }
+    if (listtype == 2) {
+      wx.navigateTo({
+        url: '../../Community/rentdetail/rentdetail?type=3&look=false'
+      })
+    }
+  },
+
+  touchStart(e){
+    if (e.touches.length == 1) {
+      this.setData({
+        //设置触摸起始点水平方向位置
+        startX: e.touches[0].clientX
+      });
+    }
+  },
+
+  touchMove(e){
+    if (e.touches.length == 1) {
+      //手指移动时水平方向位置
+      var moveX = e.touches[0].clientX;
+      //手指起始点位置与移动期间的差值
+      var disX = this.data.startX - moveX;
+      var delBtnWidth = this.data.delBtnWidth;
+      var txtStyle = "";
+      if (disX == 0 || disX < 0) {//如果移动距离小于等于0，文本层位置不变
+        txtStyle = "left:0px";
+      } else if (disX > 0) {//移动距离大于0，文本层left值等于手指移动距离
+        txtStyle = "left:-" + disX + "px";
+        if (disX >= delBtnWidth) {
+          //控制手指移动距离最大值为删除按钮的宽度
+          txtStyle = "left:-" + delBtnWidth + "px";
+        }
+      }
+      var index = e.currentTarget.dataset.index;
+      this.data.chartlist[index].txtStyle = txtStyle;
+      this.setData({
+        chartlist: this.data.chartlist
+      });
+    }
+  },
+  touchEnd(e) {
+    if (e.changedTouches.length == 1) {
+      //手指移动结束后水平位置
+      var endX = e.changedTouches[0].clientX;
+      //触摸开始与结束，手指移动的距离
+      var disX = this.data.startX - endX;
+      var delBtnWidth = this.data.delBtnWidth;
+      //如果距离小于删除按钮的1/2，不显示删除按钮
+      var txtStyle = disX > delBtnWidth / 2 ? "left:-" + delBtnWidth + "px" : "left:0px";
+      //获取手指触摸的是哪一项
+      var index = e.currentTarget.dataset.index;
+      this.data.chartlist[index].txtStyle = txtStyle;
+      this.setData({
+        chartlist: this.data.chartlist
+      });
+    }
+  },
+
+  //获取元素自适应后的实际宽度
+
+  getEleWidth(w) {
+    var real = 0;
+    try {
+      var res = wx.getSystemInfoSync().windowWidth;
+      var scale = (750 / 2) / (w / 2);//以宽度750px设计稿做宽度的自适应
+      real = Math.floor(res / scale);
+      return real;
+    } catch (e) {
+      return false;
+    }
+
+  },
+
+  initEleWidth() {
+    var delBtnWidth = this.getEleWidth(this.data.delBtnWidth);
+    this.setData({
+      delBtnWidth: delBtnWidth
+    });
+  },
+
+  //点击删除按钮事件
+  delItem(e) {
+    var that=this;
+    const index = e.target.dataset.index;
+    const listtype = parseInt(e.target.dataset.listtype);
+    var chartlist = that.data.chartlist;
+    var modalTitle="";
+    switch (listtype) {
+      case 1: modalTitle = '您确定要彻底删除此房源消息吗？'; break;
+      case 2: modalTitle = '您确定要彻底删除此室友消息吗？'; break;
+      case 3: modalTitle = '您确定要彻底删除此求租消息吗？'; break;
+      default: console.log('错误');
+    }
+    wx.showModal({
+      title: '确定',
+      confirmText: '删除',
+      confirmColor: "#f47a20",
+      cancelColor: '#4b474c',
+      content: modalTitle,
+      success: function (res) {
+        if (res.confirm) {
+          chartlist.splice(index, 1);
+          that.setData({
+            chartlist: chartlist
+          });
+        } else if (res.cancel) {
+          //用户取消删除，删除按钮隐藏
+          var txtStyle = "left:0px";
+          that.data.chartlist[index].txtStyle = txtStyle;
+          that.setData({
+            chartlist: that.data.chartlist
+          });
+        }
+      }
+    })
+
+
+
+
+    
   },
 })
 
